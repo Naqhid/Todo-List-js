c766ae9a5ae6ab35a90a5c1b7ccbd3f19a7b3ac1
Naqhid <mnaqhid@gmail.com>
2021-05-18 12:57:36 +0530

    store-stuff

--- src/project.js
+++ src/project.js
@@ -13,7 +13,7 @@ export default class Project {
   }
 
   remove(taskTitle) {
-    // Should this be a  map or reduce?
+    // Should this be a  map or reduce.?
     for (let i = 0; i < this.storage.length; i += 1) {
       if (this.storage[i].title === taskTitle) {
         this.storage.splice(i, 1);
